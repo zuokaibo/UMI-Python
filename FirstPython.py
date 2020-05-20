3064abac0b98a4100c8a1d8f5d522d4dc5f5a5ff
kzuo2 <kzuo2@wisc.edu>
2020-05-20 14:59:31 +0200

    add how to import project into new repo in github

--- FirstPython.py
+++ FirstPython.py
@@ -1,5 +1,7 @@
 print('hello python from IntelliJ')
 print('hello python from IntelliJ')
 print('with github')
-
-
+# here, you can add a new repo for new project, if you add a repo in github first,
+# then you import this project to github by a command git remote set-url <new repo
+# name> then git remote -v to check, then git pull origin master --allowed-unrelated-histories
+#
