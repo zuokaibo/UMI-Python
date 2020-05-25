a86720a3ba770a7abe1090f3ba740ade48448d87
kzuo2 <kzuo2@wisc.edu>
2020-05-25 07:28:25 +0200

    condition statement

--- FirstPython.py
+++ FirstPython.py
@@ -1,7 +1,23 @@
 print('hello python from IntelliJ')
-print('hello python from IntelliJ')
-print('with github')
+
 # here, you can add a new repo for new project, if you add a repo in github first,
 # then you import this project to github by a command git remote set-url <new repo
 # name> then git remote -v to check, then git pull origin master --allowed-unrelated-histories
-#
+# also, git clone new ssh, if you want to set up different repo for different projects
+
+x=2
+x=x+2
+print(x)
+if x > 3:
+  print('x is bigger than 3.')
+print('x is smaller.')
+if x > 8:
+  print('it is a lie.')
+print('ok, now it is working')
+if x<2:
+  print('x<2')
+elif x<3:
+  print('x<3')
+else:
+  print('x is 4')
+print('all done')
