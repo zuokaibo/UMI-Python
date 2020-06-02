fd52b64903e182698b930c6723885e6d89b89b14
kzuo2 <kzuo2@wisc.edu>
2020-06-02 21:10:07 +0200

    must from bs4 import beautifulsoup, but set up interpreter first

--- urllinks.py
+++ urllinks.py
@@ -1,10 +1,12 @@
-import urllib.request, urllib.parse, urllib.error
-import BeautifulSoup
-
+import urllib.request
+import urllib.parse
+import urllib.error
+from bs4 import BeautifulSoup
 
 url = input('Enter - ')
 html = urllib.request.urlopen(url).read()
 soup = BeautifulSoup(html, 'html.parser')
+
 
 tags = soup('a')
 for tag in tags:
