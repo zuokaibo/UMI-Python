ca84666a5d19b102c2ed8d4501893401cac58775
kzuo2 <kzuo2@wisc.edu>
2020-06-03 07:54:37 +0200

    url errors

--- urllinks.py
+++ urllinks.py
@@ -2,9 +2,16 @@ import urllib.request
 import urllib.parse
 import urllib.error
 from bs4 import BeautifulSoup
+import ssl
+
+#ctx code means ignore all errors or irregular experission
+# must put context = ctx in line 14
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter - ')
-html = urllib.request.urlopen(url).read()
+html = urllib.request.urlopen(url, context = ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
 
 
