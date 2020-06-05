c808c4b638948900c39326e48abdc9d3ff680404
kzuo2 <kzuo2@wisc.edu>
2020-06-05 10:46:08 +0200

    dir() to check list dict string legal methods

---
+++ checkmethod.py
@@ -0,0 +1,20 @@
+a = 'abc'
+type(a)
+
+b = list()
+type (b)
+
+c = dict()
+type(c)
+
+# here we can check what methods those different class have by dir()
+dir(a)
+dir(b)
+dir(c)
+
+print(dir(a))
+print(dir(b))
+print(dir(c))
+
+# also if you want to check orders , which one is bigger, which one is smaller between letters and numbers, we use
+# ord()
