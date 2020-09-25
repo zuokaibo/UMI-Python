396010443ce928aa0f524507cf333fb72cb398c9
kzuo2 <kzuo2@wisc.edu>
2020-09-25 15:08:29 +0200

    extract tag content from tag anchor

---
+++ urltags.py
@@ -0,0 +1,25 @@
+import re
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+# Retrieve all of the anchor tags
+sum=0
+tags = soup('span')
+for tag in tags:
+    # print(tag)
+    y=str(tag)
+    x= re.findall("[0-9]+",y)
+    for i in x:
+        i=int(i)
+        sum=sum+i
+print(sum)
